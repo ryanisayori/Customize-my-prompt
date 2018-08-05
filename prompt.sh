68b1dcb126b101ab1e4512b41c0a83b16385c552
7y4n1S4y0r1 <38188800+ryanisayori@users.noreply.github.com>
2018-08-05 13:45:14 +0700

    bash

---
+++ prompt.sh
@@ -0,0 +1,9 @@
+#williamgreenbygeorge@gmail.com
+#Contact me whenever you have any problems!
+#You have permission to edit this file!
+#free copyright @ 2018
+
+echo "****UPDATED****"
+
+PS1=
+PS1="\[\033[0;36m\][\u] \[\033[0;41m\]|\w|\[\033[0;35m\]=> \$\[\033[0m\]: "
